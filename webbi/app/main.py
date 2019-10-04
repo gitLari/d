13eead81ff69c86bc1c7ffed13583259e7b86121
gitLari <49396674+gitLari@users.noreply.github.com>
2019-10-04 17:38:57 +0300

    Update main.py

--- webbi/app/main.py
+++ webbi/app/main.py
@@ -3,20 +3,18 @@ import mysql.connector as mariadb
 app = Flask(__name__)
 @app.route("/")
 def hello():
-#    mariadb_connection = mariadb.connect(host='172.17.0.3', port='3306',user='tballs', password='big', database='tballs')
+    mariadb_connection = mariadb.connect(host='172.17.0.3', port='3306',user='tballs', password='big', database='tballs')
     ##mariadb_connection = mariadb.connect(user='python_user', password='some_pass', database='employees')
-#    cursor = mariadb_connection.cursor()
-#    try:
-#        cursor.execute("SELECT O_ryhmaID, nimi FROM O_ryhma WHERE O_ryhmaID=1")
+    cursor = mariadb_connection.cursor()
+    try:
+        cursor.execute("SELECT O_ryhmaID, nimi FROM O_ryhma WHERE O_ryhmaID=1")
         # where mukana:  "SELECT first_name,last_name FROM employees WHERE first_name=%s"
-#    except mariadb.Error as error:
-#        return("Virhe {}".format(error))
-#    for O_ryhmaID, nimi in cursor:
-#        rivi = ("Ryhmä: {}, Ryhmän nimi: {}").format(str(O_ryhmaID),nimi)
-#    mariadb_connection.close()
-#    return (rivi) #pois alta
-rock = "huisaa"
-return (rock)
+    except mariadb.Error as error:
+        return("Virhe {}".format(error))
+    for O_ryhmaID, nimi in cursor:
+        rivi = ("Ryhmä: {}, Ryhmän nimi: {}").format(str(O_ryhmaID),nimi)
+    mariadb_connection.close()
+    return (rivi) #pois alta
 
 if __name__ == "__main__":
     # Only for debugging while developing
